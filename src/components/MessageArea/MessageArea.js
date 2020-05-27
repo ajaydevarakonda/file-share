e5d0f4c7d0260ed0e427a1f9efbed54f468e61d1
Ajay Devarakonda <contact@ajaydevarakonda.dev>
2020-05-27 11:37:04 +0530

    Scroll to the bottom of the message box, every time you render.

--- src/components/MessageArea/MessageArea.js
+++ src/components/MessageArea/MessageArea.js
@@ -8,31 +8,43 @@ import Message from "./Message";
 
 import "./MessageArea.css";
 
-function MessageArea(props) {
-  return (
-    <div className="MessageArea">
-      <div className="MessageBox">
-        <br />
-        {props.messages.map((msg, indx) => (
-          <Message key={indx} type={msg.type} message={msg.message} />
-        ))}
+class MessageArea extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.boxRef = React.createRef();
+  }
+
+  componentDidUpdate() {
+    this.boxRef.current.scrollTop = this.boxRef.current.scrollHeight;
+  }
+
+  render() {
+    return (
+      <div className="MessageArea">
+        <div className="MessageBox" ref={this.boxRef}>
+          <br />
+          {this.props.messages.map((msg, indx) => (
+            <Message key={indx} type={msg.type} message={msg.message} />
+          ))}
+        </div>
+
+        {this.props.fileSendProgress ? (
+          <ProgressBar now={this.props.fileSendProgress} />
+        ) : (
+          ""
+        )}
+
+        {this.props.fileReceiveProgress ? (
+          <ProgressBar variant="warning" now={this.props.fileReceiveProgress} />
+        ) : (
+          ""
+        )}
+
+        <MessageInput onSubmit={this.props.onMessageSubmit} />
       </div>
-
-      {props.fileSendProgress ? (
-        <ProgressBar now={props.fileSendProgress} />
-      ) : (
-        ""
-      )}
-
-      {props.fileReceiveProgress ? (
-        <ProgressBar variant="warning" now={props.fileReceiveProgress} />
-      ) : (
-        ""
-      )}
-
-      <MessageInput onSubmit={props.onMessageSubmit} />
-    </div>
-  );
+    );
+  }
 }
 
 MessageArea.propTypes = {
